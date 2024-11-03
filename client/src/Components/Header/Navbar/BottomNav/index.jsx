25e93c67404b188691231ea6ae42d9fecb740d78
masoomeh7798 <masoomeh77ghorbani98@gmail.com>
2024-11-03 04:40:27 +0330

    basic submenu added

--- client/src/Components/Header/Navbar/BottomNav/index.jsx
+++ client/src/Components/Header/Navbar/BottomNav/index.jsx
@@ -11,28 +11,34 @@ import { FaRing } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 
-const ButtonBox={ display: 'flex', alignItems: 'center',gap:'8px' ,color:'var(--primary-clr)',fontSize:'16px',fontWeight:'500'}
+const ButtonBox = { display: 'flex', alignItems: 'center', gap: '8px', color: 'var(--primary-clr)', fontSize: '16px', fontWeight: '500' }
 
 
 export default function BottomNav() {
     return (
-        <Stack  sx={{overflowX:'scroll','::-webkit-scrollbar':{display:'none'}}} alignItems={'center'} direction={'row'} my={{ xs: 2, md: 3 }} mx={'auto'} gap={3}>
-            <Box display={{xs:'none',md:'inline-block'}} borderRadius={'8px'} >
-                <Button sx={{ py: '8px', display: 'flex', gap: '16px', bgcolor: 'var(--secondary-clr)','&:hover':{bgcolor:'var(--secondary-clr-dark)'}, '& svg': { color: 'var(--text-clr)' } }} startIcon={<IoIosMenu size={24} />} endIcon={<IoIosArrowDown size={22} />}>
+        <Stack sx={{ overflowX: 'scroll', '::-webkit-scrollbar': { display: 'none' },overflow:'visible'}} alignItems={'start'} direction={'row'} mt={{ xs: 2, md: 3 }} mx={'auto'} gap={3}>
+            <Box display={{ xs: 'none', md: 'inline-block' }} borderRadius={'8px'} >
+                <Button sx={{ py: '8px', display: 'flex', gap: '16px', bgcolor: 'var(--secondary-clr)', '&:hover': { bgcolor: 'var(--secondary-clr-dark)' }, '& svg': { color: 'var(--text-clr)' } }} startIcon={<IoIosMenu size={24} />} endIcon={<IoIosArrowDown size={22} />}>
                     <Typography fontSize={'16px'} color='var(--text-clr)' noWrap>دسته بندی ها</Typography>
                 </Button>
             </Box>
-            <List  sx={{'& li':{padding:'0'},paddingInlineStart:'0 !important'}}>
-            <Stack sx={{'& > div':{px:'16px'}}}  direction={'row'} alignItems={'center'} justifyContent={'start'} gap={{xs:1,md:3}}>
-            <ListItem><Button sx={{borderRadius:'16px',px:{xs:'8px',md:'16px'},'&:hover':{backgroundColor:'var(--secondary-clr-light)'}}} ><Link style={ButtonBox} to='/'><IoMdWatch/><span>ساعت</span></Link></Button></ListItem>
-            <ListItem><Button sx={{borderRadius:'16px',px:{xs:'8px',md:'16px'},'&:hover':{backgroundColor:'var(--secondary-clr-light)'}}} ><Link style={ButtonBox} to='/'><GiHeartNecklace/><span>گردنبند</span></Link></Button></ListItem>
-            <ListItem><Button sx={{borderRadius:'16px',px:{xs:'8px',md:'16px'},'&:hover':{backgroundColor:'var(--secondary-clr-light)'}}} ><Link style={ButtonBox} to='/'><GiDoubleNecklace/><span>سرویس</span></Link></Button></ListItem>
-            <ListItem><Button sx={{borderRadius:'16px',px:{xs:'8px',md:'16px'},'&:hover':{backgroundColor:'var(--secondary-clr-light)'}}} ><Link style={ButtonBox} to='/'><GiBigDiamondRing/><span>انگشتر</span></Link></Button></ListItem>
-            <ListItem><Button sx={{borderRadius:'16px',px:{xs:'8px',md:'16px'},'&:hover':{backgroundColor:'var(--secondary-clr-light)'}}} ><Link style={ButtonBox} to='/'><GiHeartEarrings/><span>گوشواره</span></Link></Button></ListItem>
-            <ListItem><Button sx={{borderRadius:'16px',px:{xs:'8px',md:'16px'},'&:hover':{backgroundColor:'var(--secondary-clr-light)'}}} ><Link style={ButtonBox} to='/'><FaRing/><span>دستبند</span></Link></Button></ListItem>
-            <ListItem><Button sx={{borderRadius:'16px',px:{xs:'8px',md:'16px'},'&:hover':{backgroundColor:'var(--secondary-clr-light)'}}} ><Link style={ButtonBox} to='/'><span style={{textWrap:'nowrap'}}>سایر دسته بندی ها</span></Link></Button></ListItem>
+            <List  sx={{ '& li': { padding: '0' }, paddingInlineStart: '0 !important' }}>
+                <Stack sx={{ '& > div': { px: '16px' } }} direction={'row'} alignItems={'center'} justifyContent={'start'} gap={{ xs: 1, md: 3 }}>
+                    <ListItem sx={{ position: 'relative', zIndex: '5000' }}><Button sx={{ borderRadius: '16px', px: { xs: '8px', md: '16px' }, '&:hover': { backgroundColor: 'var(--secondary-clr-light)' } }} ><Link style={ButtonBox} to='/'><IoMdWatch /><span>ساعت</span></Link></Button>
+                        <Box sx={{boxShadow:"1px 1px 2px 1px rgba(0,0,0,0.2),-1px 1px 2px 1px rgba(0,0,0,0.2)",borderRadius:'8px',position:'absolute', top:'100%', right:'0',bgcolor:'var(--text-clr)',textAlign:'start',padding:'8px 16px'  }}>
+                            <Button><Link style={{textWrap:'nowrap',color:'var(--primary-clr)'}} to='/'>ساعت زنانه</Link></Button>
+                            <Button><Link style={{textWrap:'nowrap',color:'var(--primary-clr)'}} to='/'>ساعت مردانه</Link></Button>
+                        </Box>
+                    </ListItem>
 
-            </Stack>
+                    <ListItem><Button sx={{ borderRadius: '16px', px: { xs: '8px', md: '16px' }, '&:hover': { backgroundColor: 'var(--secondary-clr-light)' } }} ><Link style={ButtonBox} to='/'><GiHeartNecklace /><span>گردنبند</span></Link></Button></ListItem>
+                    <ListItem><Button sx={{ borderRadius: '16px', px: { xs: '8px', md: '16px' }, '&:hover': { backgroundColor: 'var(--secondary-clr-light)' } }} ><Link style={ButtonBox} to='/'><GiDoubleNecklace /><span>سرویس</span></Link></Button></ListItem>
+                    <ListItem><Button sx={{ borderRadius: '16px', px: { xs: '8px', md: '16px' }, '&:hover': { backgroundColor: 'var(--secondary-clr-light)' } }} ><Link style={ButtonBox} to='/'><GiBigDiamondRing /><span>انگشتر</span></Link></Button></ListItem>
+                    <ListItem><Button sx={{ borderRadius: '16px', px: { xs: '8px', md: '16px' }, '&:hover': { backgroundColor: 'var(--secondary-clr-light)' } }} ><Link style={ButtonBox} to='/'><GiHeartEarrings /><span>گوشواره</span></Link></Button></ListItem>
+                    <ListItem><Button sx={{ borderRadius: '16px', px: { xs: '8px', md: '16px' }, '&:hover': { backgroundColor: 'var(--secondary-clr-light)' } }} ><Link style={ButtonBox} to='/'><FaRing /><span>دستبند</span></Link></Button></ListItem>
+                    <ListItem><Button sx={{ borderRadius: '16px', px: { xs: '8px', md: '16px' }, '&:hover': { backgroundColor: 'var(--secondary-clr-light)' } }} ><Link style={ButtonBox} to='/'><span style={{ textWrap: 'nowrap' }}>سایر دسته بندی ها</span></Link></Button></ListItem>
+
+                </Stack>
             </List>
         </Stack>
     )
--- client/src/Components/Header/index.jsx
+++ client/src/Components/Header/index.jsx
@@ -1,20 +1,22 @@
 import { Badge, Box, Button, Stack, Typography } from '@mui/material'
 import React from 'react'
 import Navbar from './Navbar'
+import { Link } from 'react-router-dom'
 
 
 export default function Header() {
   return (
-    <header>
+    <header style={{borderBottom:'1px solid rgba(0,0,0,0.2)'}}>
 
       {/* start top strip */}
       <Box py={1} bgcolor={'var(--secondary-clr)'} align='center'><Typography variant='body1' fontWeight={'500'} fontSize={'16px'} color='var(--text-clr)'>"عرضه به روزترين بدليجات برند و استيل ."</Typography>
       </Box>
       {/* end top strip */}
       {/* strat navbar */}
-      <Navbar />
+      <Navbar  />
       {/* end navbar */}
-
+    
+     
     </header>
   )
 }
