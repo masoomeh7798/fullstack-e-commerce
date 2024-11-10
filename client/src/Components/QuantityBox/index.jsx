9e2904fce812dcfefe623ee91a08fa0454283402
masoomeh7798 <masoomeh77ghorbani98@gmail.com>
2024-11-10 03:34:04 +0330

    cart page design with responsivity added

--- client/src/Components/QuantityBox/index.jsx
+++ client/src/Components/QuantityBox/index.jsx
@@ -5,7 +5,7 @@ import { FaMinus, FaPlus } from "react-icons/fa6";
 export default function QauntityBox() {
     const [dynamicQuantity, setdynamicQuantity] = useState(1);
     return (
-        <Stack direction={'row'} alignItems={'center'} gap={2} >
+        <Stack className='quantityChanger' direction={'row'} alignItems={'center'} gap={2} >
             <IconButton onClick={() => setdynamicQuantity(dynamicQuantity + 1)} sx={{ bgcolor: 'var(--text-clr)', boxShadow: '0 1px rgba(0,0,0,.2)', border: '1px solid rgba(0,0,0,.1)', padding: { xs: '4px', sm: '10px' } }}><FaPlus color='var(--primary-clr)' fontSize={20} /></IconButton>
             <Typography width={'30px'} textAlign={'center'} fontSize={20}>{dynamicQuantity}</Typography>
             <IconButton disabled={dynamicQuantity == 0} onClick={() => setdynamicQuantity(dynamicQuantity - 1)} sx={{ bgcolor: 'var(--text-clr)', boxShadow: '0 1px rgba(0,0,0,.2)', border: '1px solid rgba(0,0,0,.1)', padding: { xs: '4px', sm: '10px' }, "&:disabled": { opacity: .5 } }}><FaMinus color='var(--primary-clr)' fontSize={20} /></IconButton>
